82eda40d63dbd414997048e785155ee2dd1aaaa9
soumaya-elha <elhaigoune.soumaya@gmail.com>
2020-06-02 11:38:35 +0100

    commit

--- db_gestionvols.sql
+++ db_gestionvols.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.1
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : jeu. 14 mai 2020 à 20:03
+-- Généré le : mar. 02 juin 2020 à 12:33
 -- Version du serveur :  10.4.11-MariaDB
--- Version de PHP : 7.4.2
+-- Version de PHP : 7.2.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -42,7 +41,18 @@ CREATE TABLE `client` (
 --
 
 INSERT INTO `client` (`idClient`, `Nom`, `Prenom`, `Email`, `tel`, `CIN`) VALUES
-(1, 'test', 'test', 'test@gmail.com', 'SH6667', '0697546508');
+(1, 'test', 'test', 'test@gmail.com', 'SH6667', '0697546508'),
+(2, 'AAAAA', 'lllllll', 'jjjjjj@jjj.kj', 'hh1234', '0600606060'),
+(3, 'AAAAA', 'rrrrr', 'aaaaaaa@bb.col', 'hh1234', '0600606060'),
+(4, 'AAAAA', 'rrrrr', 'aaaaaaa@bb.col', '06006060606', 'hhh345'),
+(10, 'elhaigoune', 'sssss', 'aaaaaaa@bb.col', 'hh1234', '0675432187'),
+(11, '', '', '', 'hh1234', '0600606060'),
+(12, '', '', '', '', 'aaaaa'),
+(13, '', '', '', 'hh1234', '0600606060'),
+(14, 'soumaya', 'rrrrr', 'aaaaaaa@bb.col', 'hh1234', '0600606060'),
+(15, '', '', '', '', ''),
+(16, '', '', '', '', ''),
+(17, 'soumaya', 'sssss', 'aaaaaaa@bb.col', 'hh1234', '0600606060');
 
 -- --------------------------------------------------------
 
@@ -62,7 +72,48 @@ CREATE TABLE `reservation` (
 --
 
 INSERT INTO `reservation` (`idreservation`, `idClient`, `idVol`, `date_reseravtion`) VALUES
-(1, 1, 3, '2020-05-14 17:17:47');
+(1, 1, 3, '2020-05-14 17:17:47'),
+(2, 2, 2, '2020-05-18 08:19:19'),
+(3, 3, 2, '2020-05-27 12:05:30'),
+(4, 4, 2, '2020-05-27 12:05:55'),
+(5, 5, 2, '2020-05-27 12:07:07'),
+(6, 6, 2, '2020-05-27 12:11:11'),
+(7, 7, 2, '2020-05-27 12:22:43'),
+(8, 8, 2, '2020-05-27 14:33:13'),
+(9, 9, 1, '2020-05-27 16:55:52'),
+(10, 10, 2, '2020-05-31 11:46:38'),
+(11, 11, 2, '2020-05-31 11:48:57'),
+(12, 12, 2, '2020-05-31 11:49:14'),
+(13, 13, 2, '2020-05-31 11:49:30'),
+(14, 14, 1, '2020-06-01 08:43:29'),
+(15, 15, 1, '2020-06-01 08:53:59'),
+(16, 16, 1, '2020-06-01 09:17:32'),
+(17, 17, 4, '2020-06-01 10:40:53');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `userID` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `fullname` varchar(255) NOT NULL,
+  `groupeID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`userID`, `username`, `password`, `email`, `fullname`, `groupeID`) VALUES
+(1, 'meryam', '123456', 'meryam@mer.mr', '', 0),
+(2, 'aya', '111111', 'aya@aa.yy', '', 0),
+(3, 'som', '22222', 'som@so.mm', '', 1),
+(4, 'elhaig', 'AAAAA', 'aaaaaaa@bb.col', '', 0),
 
 -- --------------------------------------------------------
 
@@ -85,10 +136,10 @@ CREATE TABLE `vols` (
 --
 
 INSERT INTO `vols` (`idVol`, `depart`, `destination`, `date_depart`, `time`, `prix`, `place_disponible`) VALUES
-(1, 'Safi', 'casablanca', '2020-05-28', '12:00:00', 100, 10),
-(2, 'dakhla', 'fes', '2020-05-31', '00:00:00', 300, 10),
+(1, 'Safi', 'casablanca', '2020-05-28', '12:00:00', 100, 6),
+(2, 'dakhla', 'fes', '2020-05-31', '00:00:00', 300, -1),
 (3, 'dakhla', 'fes', '2020-06-18', '17:30:00', 300, 10),
-(4, 'fes' ,'dakhla' , '2020-06-18', '17:30:00', 300, 10);
+(4, 'fes', 'dakhla', '2020-06-18', '17:30:00', 300, 9);
 
 --
 -- Index pour les tables déchargées
@@ -107,6 +158,12 @@ ALTER TABLE `reservation`
   ADD PRIMARY KEY (`idreservation`);
 
 --
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`userID`);
+
+--
 -- Index pour la table `vols`
 --
 ALTER TABLE `vols`
@@ -120,32 +177,26 @@ ALTER TABLE `vols`
 -- AUTO_INCREMENT pour la table `client`
 --
 ALTER TABLE `client`
-  MODIFY `idClient` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `idClient` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT pour la table `reservation`
 --
 ALTER TABLE `reservation`
-  MODIFY `idreservation` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `idreservation` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT pour la table `vols`
 --
 ALTER TABLE `vols`
-  MODIFY `idVol` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `idVol` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
-
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `userID`    int(11) NOT NULL AUTO_INCREMENT,
-  `username`  varchar(255) NOT NULL,
-  `password`  varchar(255) NOT NULL,
-  `email`     varchar(255) NOT NULL,
-  `groupeID`  int(11) NOT NULL,
-  PRIMARY KEY (`userID`)
-);
-
-
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
